b660ad459465556f3ce4d6070c58ffcfb292346f
Jesse Houwing <jesse.houwing@gmail.com>
2016-02-27 18:00:56 +0100

    Sucesfully passed all parameters to the powershell file

--- vsts-extension-task/vsts-extension-task.ps1
+++ vsts-extension-task/vsts-extension-task.ps1
@@ -2,13 +2,64 @@
 param(
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
-    [string] $Itemspec = "$/",
+    [string] $PublisherID,
+    
     [Parameter(Mandatory=$true)]
-    [ValidateSet("None", "Full", "OneLevel")]
-    [string] $Recursion = "None",
+    [ValidateNotNullOrEmpty()]
+    [string] $ExtensionID,
+
     [Parameter(Mandatory=$false)]
-    [ValidateSet("true", "false")]
-    [string] $DeleteAdds = $false
+    [string] $ExtensionTag,
+
+    [Parameter(Mandatory=$false)]
+    [string] $ExtensionVersion,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("NoOverride", "Private", "PrivatePreview", "PublicPreview", "Public")]
+    [string] $ExtensionVisibility = "NoOverride",
+
+    [Parameter(Mandatory=$false)]
+    [string] $ServiceEndpoint,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("true", "false", "1", "0")]
+    [string] $TfxInstall = $false,
+
+    [Parameter(Mandatory=$false)]
+    [string] $TfxLocation = $false,
+
+    [Parameter(Mandatory=$true)]
+    [string] $EnablePackaging = $false,
+
+    [Parameter(Mandatory=$false)]
+    [string] $ManifestGlobs = "extension-manifest.json",
+
+    [Parameter(Mandatory=$false)]
+    [string] $ExtensionRoot = "$(Build.SourcesDirectory)",
+
+    [Parameter(Mandatory=$false)]
+    [string] $PackagingOutputPath = "$(Build.BinariesDirectory)",
+
+    [Parameter(Mandatory=$false)]
+    [string] $OverrideFile = "",
+
+    [Parameter(Mandatory=$false)]
+    [string] $OverrideJson = "",
+
+    [Parameter(Mandatory=$false)]
+    [string] $OverrideInternalversions = $true,
+    
+    [Parameter(Mandatory=$false)]
+    [string] $BypassValidation = $false,
+
+    [Parameter(Mandatory=$false)]
+    [string] $EnablePublishing = $false,
+
+    [Parameter(Mandatory=$false)]
+    [string] $EnableSharing = $false,
+
+    [Parameter(Mandatory=$false)]
+    [string] $ShareWith = $false
 )
 
 Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
