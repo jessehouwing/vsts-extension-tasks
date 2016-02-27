9cb1c4bd15ed3d76c1c10c5cefc03957661f360c
Jesse Houwing <jesse.houwing@gmail.com>
2016-02-27 13:48:47 +0100

    Setup extension manifest to support all package operations

--- vsts-extension-task/vsts-extension-task.ps1
+++ vsts-extension-task/vsts-extension-task.ps1
@@ -1,1 +1,19 @@
-﻿
+﻿[cmdletbinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Itemspec = "$/",
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("None", "Full", "OneLevel")]
+    [string] $Recursion = "None",
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("true", "false")]
+    [string] $DeleteAdds = $false
+)
+
+Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+$PSBoundParameters.Keys | %{ Write-Verbose "$_ = $($PSBoundParameters[$_])" }
+
+Write-Verbose "Importing modules"
+Import-Module -DisableNameChecking "$PSScriptRoot/vsts-extension-shared.psm1"
