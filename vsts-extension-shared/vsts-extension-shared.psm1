0de52f49748503414d0649c1966ec90b64642e67
Jesse Houwing <jesse.houwing@gmail.com>
2016-06-08 12:29:41 +0200

    Adds basic auth server uri and additionalArguments support

--- vsts-extension-shared/vsts-extension-shared.psm1
+++ vsts-extension-shared/vsts-extension-shared.psm1
@@ -13,6 +13,7 @@ function Convert-GlobalOptions
         TfxUpdate = ($parameters["TfxUpdate"] -eq $true)
         TfxLocation = [string]$parameters["TfxLocation"]
         ServiceEndpoint = [string]$parameters["ServiceEndpoint"]
+        TfxArguments = [string]$parameters["TfxArguments"]
     }
 
     Write-Debug "GlobalOptions:"
@@ -406,6 +407,8 @@ function Invoke-Tfx
                 $Arguments += $ServiceEndpoint.Authorization.Parameters.Username
                 $Arguments += "--password"
                 $Arguments += $ServiceEndpoint.Authorization.Parameters.Password
+                $Arguments += "--service-url"
+                $Arguments += $ServiceEndpoint.Authorization.Parameters.serverUri
             }
             "Token"
             {
@@ -425,6 +428,11 @@ function Invoke-Tfx
      
 
     $tfxArgs = ($Arguments | %{ Escape-Args $_ } ) -join " "
+
+    if ($global:globalOptions.TfxArguments)
+    {
+        $tfxArgs += " $($global:globalOptions.TfxArguments)"
+    }
 
     Write-Debug "Calling: $($global:tfx)"
     Write-Debug "Arguments: $tfxArgs"
